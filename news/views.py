a6775be9809f82a2c0891608066f504091863332
unknown <idirisovasezim2005@gmail.com>
2023-09-04 19:25:58 +0600

    created templates

--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import News
 
-# Create your views here.
+def main(request):
+    a = News.objects.all()
+    context = {'queryset' : a}
+    return render(request, 'index.html', context)
+
+def content(request):
+    ...
+
