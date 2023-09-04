ac7d10409e5832939ebb29ee9bd85bab994e30a3
unknown <idirisovasezim2005@gmail.com>
2023-09-04 16:35:27 +0600

    created models

--- news/models.py
+++ news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='news_image')
+    created_time = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f'{self.title}'
